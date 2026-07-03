f8f17f97ed1cdd9383b6d53af26616e0280e2f6e
abhishekraj21108 <abhishekrajkatihar1@gmail.com>
2026-07-04 04:40:32 +0530

    Add welcome alert on button click

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+document.querySelector("button").addEventListener("click",function(){
+alert("Welcome to CG1 HOMEWORK Portal!\n\nHomework, Study Materials, Revision Quizzes and Udayam AI will be available soon.\n\nOperated by Abhishek Raj 🚀");
+});
